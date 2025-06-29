bbc57b91496ff4b608fda7c49a0cfcb422df2a11
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2025-06-28 22:26:45 -0300

    feat: add Tailwind color rendering to main.js

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-import { Colors } from "./colors.js";
+import { Tailwind, Colors } from "./colors.js";
 
-Object.keys(Colors).forEach(key => {
-  const color = Colors[key]
+Object.keys(Tailwind).forEach(key => {
+  const color = Tailwind[key]
 
   const cardElement = document.createElement('card-color')
-  cardElement.setAttribute('color', color)
+  if (color) {
+    const name = key.substring(0, 1).toUpperCase() + key.substring(1, key.length).replace('_', ' ')
 
-  document.querySelector('main').appendChild(cardElement)
+    cardElement.setAttribute('color', color)
+    cardElement.setAttribute('tooltip', `${name}\\n${color}`)
+  }
+
+  document.querySelector('#tailwind').appendChild(cardElement)
+})
+
+Object.keys(Colors).forEach(key => {
+  const color = Colors[key]?.toUpperCase()
+
+  const cardElement = document.createElement('card-color')
+  if (color) {
+    const name = key.substring(0, 1).toUpperCase() + key.substring(1, key.length).replace('_', ' ')
+
+    cardElement.setAttribute('color', color.toUpperCase())
+    cardElement.setAttribute('tooltip', `${name}\\n${color}`)
+  }
+
+  document.querySelector('#others').appendChild(cardElement)
 })
